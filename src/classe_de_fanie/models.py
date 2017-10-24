e816523d1c9006e06cb4d40deb9efd6a716277e7
Jerome <jerome.verdoni@code3.ca>
2017-10-23 20:17:14 -0400

    basic

--- src/classe_de_fanie/models.py
+++ src/classe_de_fanie/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Eleve(models.Model):
+    nom_eleve = models.TextField(max_length=50)
+    prenom_eleve = models.TextField(max_length=50)
+    nom_parents_1 = models.TextField(max_length=50)
+    prenom_parents_1 = models.TextField(max_length=50)
+    telephone_parents_1 = models.TextField(max_length=50)
+    courriel_parents_1 = models.TextField(max_length=75)
+    nom_parents_2 = models.TextField(max_length=50)
+    prenom_parents_2 = models.TextField(max_length=50)
+    telephone_parents_2 = models.TextField(max_length=50)
+    courriel_parents_2 = models.TextField(max_length=75)
+    photo = models.TextField(max_length=75, default="none")
+
+    def __str__(self):
+        return self.prenom_eleve + " " + self.nom_eleve
--- src/site_web/settings.py
+++ src/site_web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SETTINGS_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['classedefanie.site']
+ALLOWED_HOSTS = ['*','classedefanie.site','localhost']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'classe_de_fanie.apps.ClasseDeFanieConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = ( os.path.join('static'), )
