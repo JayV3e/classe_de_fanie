920115e135a4e0636cb171f3f685c97afd114658
Jerome <jerome.verdoni@code3.ca>
2017-11-08 19:01:08 -0500

    Password

--- src/site_web/settings.py
+++ src/site_web/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'classe_de_fanie.apps.ClasseDeFanieConfig',
-    #'lockdown',
+    'lockdown',
 ]
 
 MIDDLEWARE = [
