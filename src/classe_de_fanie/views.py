47cd61c2972bdf1e42fb3ce6cf788cd66069fd02
Jerome <jerome.verdoni@code3.ca>
2017-11-08 19:10:18 -0500

    removing lockdown

--- src/classe_de_fanie/views.py
+++ src/classe_de_fanie/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Eleve
 
-from lockdown.decorators import lockdown
+#from lockdown.decorators import lockdown
 
 # Create your views here.
-@lockdown()
+#@lockdown()
 def index(request):
     template = 'index.html'
     eleves = Eleve.objects.order_by('non_enseignant','prenom_eleve')
